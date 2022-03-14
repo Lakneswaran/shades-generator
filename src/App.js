e28a2f56094ca45592b263bd9ae050655898cea3
Lakneswaran <90680982+Lakneswaran@users.noreply.github.com>
2022-03-14 20:37:35 +0100

    imported libraries in app comp.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import {useEffect, useState} from "react";
+import ChromeColor from '@uiw/react-color-chrome';
+import values from 'values.js';
+import {CopyToClipboard } from 'react-copy-to-clipboard';
+
 
 function App() {
   return (
    <>
-    <h1 class="text-gray-700 font-mono text-lg text-center bg-gray-400 px-4 py-2 m-2">Hello</h1>
-    <p>Hello</p>
+    <div>
+      
+    </div>
    </>
   );
 }

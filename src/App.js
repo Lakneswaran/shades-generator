848617955f4f649cc86be74b666dc6caa1458988
Lakneswaran <90680982+Lakneswaran@users.noreply.github.com>
2022-03-14 20:12:35 +0100

    all installation done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+   <>
+    <h1 class="text-gray-700 font-mono text-lg text-center bg-gray-400 px-4 py-2 m-2">Hello</h1>
+    <p>Hello</p>
+   </>
+  );
+}
+
+export default App;
